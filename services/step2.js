7aa9bb08941e821ec06096a43428718204cdc379
henry <henry@Macbook-Henry.homenet.telecomitalia.it>
2021-03-24 00:34:04 +0100

    Fix Step 2 function.

--- services/step2.js
+++ services/step2.js
@@ -1,7 +1,8 @@
 /**
  * Step 2 (two sentence).
  * Assuming that:
- *  - new line char is the separator
+ *  - new line char \n is the separator
+ *  - customer and agent are always not equal strings
  * 
  **/
 
@@ -11,7 +12,7 @@ const ChatItem = require('../models/ChatItem');
 //this is the function from Step1, cloned to enhanced the steps for this exercise ;-)
 // it could be defined once following coding best practise.
 
-const parseSingleSentence = (singleSentence) =>{
+const parseSingleSentence = (singleSentence, customerUsername) =>{
 
     let chatItem;//new ChatItem();
 
@@ -28,13 +29,22 @@ const parseSingleSentence = (singleSentence) =>{
 
         //use ChatItem constructor
         //chatItem= new ChatItem(dateTime, mentionStr, sentenceStr, 'customer');
+
+        let typeStr = 'customer';
+        //console.log('Customer user name extracted is: ' + customerUsername);
+        //console.log('Customer user name now is: ' + mentionStr.slice(9, mentionStr.length));
+
+        if (mentionStr.slice(9, mentionStr.length) != customerUsername) {
+            typeStr = 'agent';
+        }
+
         chatItem= {
             date: dateTime,
             mention: mentionStr,
             sentence: sentenceStr,
-            type: 'customer'
+            type: typeStr
         };
-        //console.log('chat item type is... ' + chatItem.type);
+        console.log('chat item type is... ' + chatItem.type);
     }
     return chatItem;
 }
@@ -42,12 +52,18 @@ const parseSingleSentence = (singleSentence) =>{
 const parseChatSentences = (chatSentences) =>{
     let chat = new Chat();
 
+    //check empty string...
+
     //split chat sentences thanks to the new line char
     let chatItems = chatSentences.split("\n");
 
+    //define once the customer username string
+    let mentionStrLength = chatItems[0].split(": ", 1).join(": ").length + 2;
+    let customerUsername = chatItems[0].slice(9, mentionStrLength);
+
     //for each item of chatItems
     chatItems.forEach(element => {
-        let chatItem = parseSingleSentence(element);
+        let chatItem = parseSingleSentence(element, customerUsername);
         chat.items.push(chatItem);
       });
 
