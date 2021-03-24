f27a5ede0ae29dc5b2b7eabafe546f82fee7d32c
efietta <henry@Macbook-Henry.homenet.telecomitalia.it>
2021-03-24 23:29:56 +0100

    Step 5 test and Step 6 test work fine.

---
+++ test/step6.test.js
@@ -0,0 +1,15 @@
+/**
+ * Step 6 (ignore extra date)
+ * 
+ */
+
+const parseChatSentences = require("../services/step5");
+const Chat = require('../models/Chat');
+
+describe ("Retrieve data from the chat with multiple sentences", () =>  {
+    test('split in two using date splitting ignoring extra dates', () =>{
+        let sentences = "14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.14:26:15 Agent : I received it at 12:24:48, ut blandit lectus.";
+        expect(parseChatSentences(sentences).length).toEqual(2);
+    });
+
+});
